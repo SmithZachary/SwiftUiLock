cea8980b612c94248b31e0a1df2f30f664df98f3
Zachary Smith <zachary.6.smith@gmail.com>
2021-09-06 16:56:32 -0500

    Initial Commit

---
+++ Lockday10/Lockday10App.swift
@@ -0,0 +1,17 @@
+//
+//  Lockday10App.swift
+//  Lockday10
+//
+//  Created by Zach Smith on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Lockday10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
